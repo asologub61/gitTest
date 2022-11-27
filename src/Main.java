a895322696214fb8dc732c44e0f6a53c0459f088
asologub61 <andrey-s-workmail@yandex.ru>
2022-11-27 22:44:37 +0300

    added method in SecondClass

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
     SecondClass sC = new SecondClass();
         System.out.println(sC.hW);
+        sC.vision(sC.hW, sC.gW);
+
 
     }
 }
--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,4 +1,8 @@
 public class SecondClass {
     String hW = "Hello World!";
     String gW = "Goodbye World (";
+
+    public void vision(String hW, String gW){
+        System.out.println(hW + gW);
+    }
 }
